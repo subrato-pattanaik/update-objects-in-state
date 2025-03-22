e781dc85459d881e7daeb67c95b2bc135b5e6291
subrato-pattanaik <subrato.pattanaik@gmail.com>
2025-03-22 14:11:38 +0530

    downgrade react to support spectacle

--- src/App.tsx
+++ src/App.tsx
@@ -7,23 +7,36 @@ import {
   SpectacleLogo,
 } from "spectacle";
 
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
 function App() {
   return (
-    <>
-      <Deck template={() => <DefaultTemplate />}>
-        <Slide>
-          <FlexBox height="100%">
-            <Heading>test</Heading>
-          </FlexBox>
-        </Slide>
-        <Slide>
-          <FlexBox height="100%">
-            <Heading fontSize="h2">Made with</Heading>
-            <SpectacleLogo size={300} />
-          </FlexBox>
-        </Slide>
-      </Deck>
-    </>
+    <Deck template={() => <DefaultTemplate />}>
+      <Slide>
+        <FlexBox height="100%" flexDirection="column">
+          <a
+            href="https://react.dev/learn/updating-objects-in-state"
+            target="_blank"
+          >
+            <img
+              height={300}
+              width={300}
+              src={reactLogo}
+              className="logo react"
+              alt="React logo"
+            />
+          </a>
+          <Heading>Update objects in state</Heading>
+        </FlexBox>
+      </Slide>
+      <Slide>
+        <FlexBox height="100%" justifyContent="center" alignItems="center">
+          <Heading fontSize="h2">Made with</Heading>
+          <SpectacleLogo size={300} />
+        </FlexBox>
+      </Slide>
+    </Deck>
   );
 }
 
