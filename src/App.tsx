44687af5226a33a161b78229ab7183772dececb0
subrato-pattanaik <subrato.pattanaik@gmail.com>
2025-03-22 13:04:06 +0530

    add spectacle

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,28 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-
-import "./App.css";
+import {
+  Deck,
+  DefaultTemplate,
+  FlexBox,
+  Heading,
+  Slide,
+  SpectacleLogo,
+} from "spectacle";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Deck template={() => <DefaultTemplate />}>
+        <Slide>
+          <FlexBox height="100%">
+            <Heading>test</Heading>
+          </FlexBox>
+        </Slide>
+        <Slide>
+          <FlexBox height="100%">
+            <Heading fontSize="h2">Made with</Heading>
+            <SpectacleLogo size={300} />
+          </FlexBox>
+        </Slide>
+      </Deck>
     </>
   );
 }
